bf4994e528a3fbaec13a2c4d8b79cbe2cbb4ccad
magiclin <magiclin89@gmail.com>
2013-08-31 20:56:15 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magicurls::Application.config.secret_token = '07ad6c89686e328995e127571c6c423275e981a0c2a3219b0e13004dfbc9d084b3c86a139169df96b0ad8bf5fda9a09e24d231d10f33eec6fed916750c37c7f8'
